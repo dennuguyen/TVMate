94187dd5184f06484c94530307c2d07498719d51
dennuguyen <dennuguyen@gmail.com>
2024-06-05 18:02:15 +1000

    Remove unused provider.

--- lib/src/add_remote/add_remote_container.dart
+++ lib/src/add_remote/add_remote_container.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:tvmate/src/add_remote/add_remote_form/add_remote_form.dart';
 
 class AddRemoteContainer extends StatelessWidget {
@@ -7,15 +6,10 @@ class AddRemoteContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Consumer(
-      builder: (BuildContext context, remotes, Widget? child) {
-        return const Dialog(
-          child: Padding(
-            padding: EdgeInsets.all(8.0),
-            child: AddRemoteForm(),
-          ),
-        );
-      },
+    // TODO: UI bug where backgrounds are slightly diff colour due to padding.
+    return const Padding(
+      padding: EdgeInsets.all(16.0),
+      child: AddRemoteForm(),
     );
   }
 }
