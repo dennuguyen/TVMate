c524ac5d2658cc78a3eddcaa3e82d4104597b94a
dennuguyen <dennuguyen@gmail.com>
2024-06-08 23:26:28 +1000

    Add and comment floorPlan Map.

--- lib/src/floor_plan/floor_plan.dart
+++ lib/src/floor_plan/floor_plan.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:tvmate/src/draggable_canvas/draggable_canvas.dart';
 
 class FloorPlan extends StatelessWidget {
+  // Map<String, _> floorPlan;
+
   const FloorPlan({super.key});
 
   @override
