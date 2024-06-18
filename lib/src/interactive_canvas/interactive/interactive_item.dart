d74918069e44c9a856e7d59fd70e58c8cac45255
dennuguyen <dennuguyen@gmail.com>
2024-06-19 00:07:20 +1000

    Let items know if they're selectable (basic collision behaviour).

--- lib/src/interactive_canvas/interactive/interactive_item.dart
+++ lib/src/interactive_canvas/interactive/interactive_item.dart
@@ -5,12 +5,14 @@ import 'package:tvmate/src/interactive_canvas/interactive/shapeable.dart';
 
 abstract class InteractiveItem extends CustomPainter
     implements Interactable, Shapeable, Copyable {
-  late double _initialAngle;
-  late double _initialScale;
+  late double _initialAngle = 0.0;
+  late double _initialScale = 1.0;
+  bool selected = false;
 
   @override
   void onScaleStart(ScaleStartDetails details) {
     if (contains(details.localFocalPoint)) {
+      selected = true;
       _initialAngle = angle;
       _initialScale = scale;
     }
@@ -18,13 +20,16 @@ abstract class InteractiveItem extends CustomPainter
 
   @override
   void onScaleUpdate(ScaleUpdateDetails details) {
-    position = details.localFocalPoint;
-    angle = details.rotation + _initialAngle;
-    scale = details.scale * _initialScale;
+    if (selected == true) {
+      position = details.localFocalPoint;
+      angle = details.rotation + _initialAngle;
+      scale = details.scale * _initialScale;
+    }
   }
 
   @override
   void onScaleEnd(ScaleEndDetails details) {
+    selected = false;
     _initialAngle = 0.0;
     _initialScale = 1.0;
   }
