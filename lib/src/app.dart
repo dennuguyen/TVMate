4cc6ccd283b2b6acae47692d5e4aa9695f98b626
dennuguyen <dennuguyen@gmail.com>
2024-07-01 19:57:07 +1000

    Clean up ChangeNotifierProvider in app.dart.

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -3,9 +3,6 @@ import 'package:provider/provider.dart';
 import 'package:tvmate/src/add_remote/add_remote_form_controller.dart';
 import 'package:tvmate/src/add_remote/open_add_remote_modal_button.dart';
 import 'package:tvmate/src/device_list/device_list_controller.dart';
-import 'package:tvmate/src/interactive_canvas/interactive/interactive_canvas_controller.dart';
-import 'package:tvmate/src/interactive_canvas/canvas/raw_canvas_controller.dart';
-import 'package:tvmate/src/interactive_canvas/snap_controller.dart';
 import 'package:tvmate/src/floor_plan/open_add_floor_plan_modal_button.dart';
 import 'package:tvmate/src/remote/remote_list.dart';
 import 'package:tvmate/src/remote/remote_list_controller.dart';
@@ -16,16 +13,11 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // ChangeNotifierProvider must be placed at root because of Navigator.
-    // TODO: move controllers closer to their use (esp. canvas ones).
     return MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (context) => RemoteListController()),
         ChangeNotifierProvider(create: (context) => DeviceListController()),
         ChangeNotifierProvider(create: (context) => AddRemoteFormController()),
-        ChangeNotifierProvider(create: (context) => SnapController()),
-        ChangeNotifierProvider(
-            create: (context) => InteractiveCanvasController()),
-        ChangeNotifierProvider(create: (context) => RawCanvasController()),
       ],
       child: MaterialApp(
         theme: ThemeData(
